ed8179c1069694d5c2e5821716b22721ff72db54
IAMLATHIENTU <166508045+IAMLATHIENTU@users.noreply.github.com>
2024-05-11 13:34:21 +0000

    create route counsel and create file createCounsel.ejs with action use to create a post of counsel

--- src/controllers/counsel.controller.js
+++ src/controllers/counsel.controller.js
@@ -4,6 +4,10 @@ class controller {
     TuVanBackEndFrontEnd(req,res) {
         return res.render('counsel/tu-van-hoc-be-hay-fe.ejs')
     }
+
+    createCounsel(req,res) {
+        return res.render('createCounsel.ejs')
+    }
 }
 
 module.exports = new controller
--- src/routes/counsel.route.js
+++ src/routes/counsel.route.js
@@ -3,4 +3,6 @@ const controller = require('../controllers/counsel.controller')
 
 router.get('/tu-van-hoc-be-hay-fe', controller.TuVanBackEndFrontEnd)
 
+router.get('/create', controller.createCounsel)
+
 module.exports = router
